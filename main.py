c2dddfed62baa31cfd80fb4160f728a32e10a81d
lightyears1998 <lightyears1998@hotmail.com>
2021-11-26 20:23:51 +0800

    Sprinkle some love and magic

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import requests
 import os
 import yaml
@@ -5,6 +6,8 @@ try:
     from yaml import CLoader as YamlLoader, CDumper as YamlDumper
 except ImportError:
     from yaml import YamlLoader, YamlDumper
+import time
+import random
 
 API = os.getenv("NETEASE_API") or "http://localhost:3000"
 RAW_COOKIE = os.getenv("NETEASE_COOKIE") or \
@@ -24,7 +27,7 @@ def get_path(path):
 def bootstrap():
     print("Using DATA_DIR", DATA_DIR)
 
-    dirs = ["songs", "playlists"]
+    dirs = ["lyrics", "playlists"]
     dirs = list(map(lambda dir: get_path(dir), dirs))
     for dir in dirs:
         os.makedirs(dir, exist_ok=True)
@@ -50,27 +53,41 @@ def get_playlist(playlist_id: int) -> list[int]:
     if cache:
         return cache
     else:
-        out = requests.post(API + "/playlist/detail", {
+        res = requests.post(API + "/playlist/detail", {
             "id": str(playlist_id),
             "cookie": cookie
         })
 
-        song_list = out.json()
+        song_list = res.json()
         return cache_playlist(playlist_id, song_list)
 
 def cache_lyrics(song_id: int, lyrics=None):
     path = get_path("lyrics/{}.yaml".format(song_id))
     return cache(path, lyrics)
 
-def get_lyrics(song_id: int) -> None:
-    out = requests.post(API + "/lyric", {
-        "id": str(song_id)
-    }).json()
-    lrc = out['lrc']
-    if lrc and lrc['lyric']:
-        print(lrc['lyric'])
+def get_lyrics(song_id: int) -> Tuple[dict, bool]:
+    cache = cache_lyrics(song_id)
+    if cache:
+        return cache, True
+    else:
+        res = requests.get(API + "/lyric?id={}".format(song_id))
+        lyrics = res.json()
+        return cache_lyrics(song_id, lyrics), False
 
-if __name__ == "__main__":
+def expand_list_and_get_lyrics(list):
+    track_ids = list["playlist"]["trackIds"]
+    total = len(track_ids)
+    for i in range(total):
+        song_id = track_ids[i]["id"]
+        print("processing {} of {}, song_id={}".format(i+1, total, song_id))
+        _, cache_hit = get_lyrics(song_id)
+        if not cache_hit:
+            time.sleep(random.random() * 5)
+
+def main():
     bootstrap()
     list = get_playlist(162317931)
-    # get_lyrics(572468962)
+    expand_list_and_get_lyrics(list)
+
+if __name__ == "__main__":
+    main()
